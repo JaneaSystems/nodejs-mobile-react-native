62c2670b743bc60da817e8a686622fad597f1737
Jaime Bernardo <jaime@janeasystems.com>
2018-01-24 19:48:14 +0000

    ios: native modules support
    Adds run scripts phases to the XCode project to build and sign
    npm native modules. These can be activated by setting the
    environment variable 'NODEJS_MOBILE_BUILD_NATIVE_MODULES' to 1.

--- scripts/module-postlink.js
+++ scripts/module-postlink.js
@@ -143,6 +143,49 @@ if ( detectedConfigs && detectedConfigs.ios && detectedConfigs.ios.pbxprojPath)
       'ENABLE_BITCODE','NO','Release'
     );
 
+    //Adds a build phase to rebuild native modules
+    var rebuildNativeModulesBuildPhaseName = 'Build NodeJS Mobile Native Modules';
+    var rebuildNativeModulesBuildPhaseScript = `
+if [ "1" != "$NODEJS_MOBILE_BUILD_NATIVE_MODULES" ]; then exit 0; fi
+set -e
+NODEJS_HEADERS_DIR="$( cd "$PROJECT_DIR" && cd ../node_modules/nodejs-mobile-react-native/ios/libnode/ && pwd )"
+pushd $CODESIGNING_FOLDER_PATH/nodejs-project/
+if [ "$PLATFORM_NAME" == "iphoneos" ]
+then
+  GYP_DEFINES="OS=ios" npm_config_nodedir="$NODEJS_HEADERS_DIR" npm_config_platform="ios" npm_config_node_engine="chakracore" npm_config_arch="arm64" npm --verbose rebuild --build-from-source
+else
+  GYP_DEFINES="OS=ios" npm_config_nodedir="$NODEJS_HEADERS_DIR" npm_config_platform="ios" npm_config_node_engine="chakracore" npm_config_arch="x64" npm --verbose rebuild --build-from-source
+fi
+popd
+`
+    var rebuildNativeModulesBuildPhase = xcodeProject.buildPhaseObject('PBXShellScriptBuildPhase', rebuildNativeModulesBuildPhaseName, firstTargetUUID);
+    if (!(rebuildNativeModulesBuildPhase)) {
+      xcodeProject.addBuildPhase(
+        [],
+        'PBXShellScriptBuildPhase',
+        rebuildNativeModulesBuildPhaseName,
+        firstTargetUUID,
+        { shellPath: '/bin/sh', shellScript: rebuildNativeModulesBuildPhaseScript }
+      );
+    }
+
+    //Adds a build phase to sign native modules
+    var signNativeModulesBuildPhaseName = 'Sign NodeJS Mobile Native Modules';
+    var signNativeModulesBuildPhaseScript = `
+if [ "1" != "$NODEJS_MOBILE_BUILD_NATIVE_MODULES" ]; then exit 0; fi
+/usr/bin/codesign --force --sign $EXPANDED_CODE_SIGN_IDENTITY --preserve-metadata=identifier,entitlements,flags --timestamp=none $(find "$CODESIGNING_FOLDER_PATH/nodejs-project/" -type f -name "*.node")
+`
+    var signNativeModulesBuildPhase = xcodeProject.buildPhaseObject('PBXShellScriptBuildPhase', signNativeModulesBuildPhaseName, firstTargetUUID);
+    if (!(signNativeModulesBuildPhase)) {
+      xcodeProject.addBuildPhase(
+        [],
+        'PBXShellScriptBuildPhase',
+        signNativeModulesBuildPhaseName,
+        firstTargetUUID,
+        { shellPath: '/bin/sh', shellScript: signNativeModulesBuildPhaseScript }
+      );
+    }
+
     //Writes the updated .pbx file
     fs.writeFileSync(pbxProjectPath,xcodeProject.writeSync(),'utf-8');
   });
