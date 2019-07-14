c33fd8f3f81ba3dc530dd2a5d7957569c30775e9
is343 <anz17n@gmail.com>
2019-07-14 14:58:47 +0900

    add typescript type definitions

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+declare module "nodejs-mobile-react-native" {
+  export interface NodeJs {
+    start: (scriptFileName: string, options?: StartupOptions) => void
+    startWithScript: (scriptBody: string, options?: StartupOptions) => void
+    channel: Channel;
+  }
+  export interface Channel {
+    addListener: (event: string, message: string) => void;
+    post: (event: string, message: string) => void;
+    send: (message: string) => void;
+  }
+
+  export interface StartupOptions {
+    redirectOutputToLogcat?: boolean
+  }
+
+  const nodejs: NodeJs
+
+  export default nodejs
+}
